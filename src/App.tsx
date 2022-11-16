ec136415a744bdeb411c5feeccb026b8ca8d8d65
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-11-15 16:58:12 -0800

    "First"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Box} from "./components/views/box";
+import { Secondview } from "./components/views/secondview";
+
+export const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Box/>}/>
+      <Route path="/secondview" element={<Secondview/>}/>
+    </Routes>
   );
 }
-
-export default App;
