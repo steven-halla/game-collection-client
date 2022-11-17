66d06ee98b75511b17d621601b740dd007e38e2c
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-11-16 17:27:53 -0800

    "First"

---
+++ src/services/authHeader.ts
@@ -0,0 +1,10 @@
+export const authHeader = () => {
+
+  const userAuth = JSON.parse(<string>localStorage.getItem("userAuth"));
+
+  if (userAuth && userAuth.accessToken) {
+    return {'authorization': userAuth.accessToken};
+  } else {
+    return {}
+  }
+}
