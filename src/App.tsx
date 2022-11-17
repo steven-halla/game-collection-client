cd94bf0b52802b28d80c90f6efb5f7641c544037
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-11-16 17:23:20 -0800

    "First"

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Box} from "./components/views/box";
 import { Secondview } from "./components/views/secondview";
+import { Signin } from "./components/views/signin";
 
 export const App = () => {
 
@@ -11,6 +12,7 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<Box/>}/>
       <Route path="/secondview" element={<Secondview/>}/>
+      <Route path="/signin" element={<Signin/>}/>
     </Routes>
   );
 }
